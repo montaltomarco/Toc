72eb79c797d7ee057b136e38d76c1256b611bce8
arcticless <victor.cojocaru@insa-lyon.fr>
2015-05-06 19:10:19 +0200

    bien bien bien

--- app/toc/utils.py
+++ app/toc/utils.py
@@ -14,24 +14,33 @@ def getCoordByNames(firstAddress, secondAddress):
     temp = []
     temp2 = []
     response_data = {}
-
+    i = 0
     resp1 = requests.get(u"http://nominatim.openstreetmap.org/search/?q=lyon+%s&format=json" %firstAddress).json()
+    print resp1
     for o in resp1:
-        dict2={}
-        dict2[u'lon']= o['lon']
-        dict2[u'lat']= o['lat']
-        dict2[u'name']= o['display_name']
-        temp.append(dict2)
-    dict[u'firstAddress'] = temp
+        i = i+1
+        if i<5:
+            dict2={}
+            dict2[u'lon']= o['lon']
+            dict2[u'lat']= o['lat']
+            dict2[u'name']= o['display_name'].split(",")[:3]
+            temp.append(dict2)
+        else:
+            pass
+    i=0
 
     r2 = requests.get(u"http://nominatim.openstreetmap.org/search/?q=lyon+%s&format=json" %secondAddress).json()
     for o in r2:
-        dict4={}
-        dict4[u'lon']= o['lon']
-        dict4[u'lat']= o['lat']
-        dict4[u'name']= o['display_name']
-        temp2.append(dict4)
-    response_data['firstAddress'] = temp
+        i=i+1
+        if i<5:
+            dict4={}
+            dict4[u'lon']= o['lon']
+            dict4[u'lat']= o['lat']
+            dict4[u'name']= o['display_name'].split(",")[:3]
+            temp2.append(dict4)
+        else:
+            pass
+    
     response_data['secondAddress'] = temp2
-
+    response_data['firstAddress'] = temp
     return json.dumps(response_data)
